1129f3509cc588a38f369e3c59b596ab64fc18d2
mraliu <teachmrliu@gmail.com>
2024-05-17 22:24:22 +0100

    Change frames

--- main.py
+++ main.py
@@ -68,26 +68,55 @@ root = tk.Tk()
 root.title("DB window")
 root.geometry("800x600")
 
-search_frame = tk.Frame(root, bg="Black")
-search_frame.place(x=0,y=50, width=800, height=550)
+from tkinter.filedialog import askopenfilename
+
+def searchPlayerFrame():
+    frames[0].place(x=0,y=50, width=800, height=550)
+
+def anotherFrame():
+    frames[0].place_forget()
+
+def About():
+    print("This is a simple example of a menu")
+    
+menu = tk.Menu(root)
+root.config(menu=menu)
+
+# Menus
+filemenu = tk.Menu(menu)
+helpmenu = tk.Menu(menu)
+
+menu.add_cascade(label="File", menu=filemenu)
+filemenu.add_command(label="Search Player", command=searchPlayerFrame)
+filemenu.add_command(label="Another Frame", command=anotherFrame)
+filemenu.add_command(label="Exit", command=root.quit)
+
+menu.add_cascade(label="Help", menu=helpmenu)
+helpmenu.add_command(label="About...", command=About)
+                     
+# The frames                     
+frames = [tk.Frame(root)]
+
+# Places inital frames
+frames[0].place(x=0,y=50, width=800, height=550)
 
 # Tk Elements
-scrollbarv = tk.Scrollbar(search_frame, orient='vertical')
-scrollbarh = tk.Scrollbar(search_frame, orient='horizontal')
+scrollbarv = tk.Scrollbar(frames[0], orient='vertical')
+scrollbarh = tk.Scrollbar(frames[0], orient='horizontal')
 
-gkbtn = tk.Button(search_frame, text="Goalkeeper", command=lambda x="gk":filterdb(x), bg="Green")
+gkbtn = tk.Button(frames[0], text="Goalkeeper", command=lambda x="gk":filterdb(x), bg="Green")
 gkbtn.place(x = 50, y = 75, width = 100, height = 20)
 
-defbtn = tk.Button(search_frame, text="Defender", command=lambda x="def":filterdb(x), bg="Red")
+defbtn = tk.Button(frames[0], text="Defender", command=lambda x="def":filterdb(x), bg="Red")
 defbtn.place(x = 150, y = 75, width = 100, height = 20)
 
-midbtn = tk.Button(search_frame, text="Midfielder", command=lambda x="mid":filterdb(x), bg="Aqua")
+midbtn = tk.Button(frames[0], text="Midfielder", command=lambda x="mid":filterdb(x), bg="Aqua")
 midbtn.place(x = 250, y = 75, width = 100, height = 20)
 
-attbtn = tk.Button(search_frame, text="Attack", command=lambda x="att":filterdb(x), bg="Brown")
+attbtn = tk.Button(frames[0], text="Attack", command=lambda x="att":filterdb(x), bg="Brown")
 attbtn.place(x = 350, y = 75, width = 100, height = 20)
 
-treeview = ttk.Treeview(search_frame, columns=heading[1:])
+treeview = ttk.Treeview(frames[0], columns=heading[1:])
 
 for idx, head in enumerate(heading):
     if idx==0:
@@ -105,22 +134,19 @@ scrollbarh.place(x=50, y=275, width=700)
 scrollbarv.config(command = treeview.yview)
 scrollbarh.config(command = treeview.xview)
 
-searchlbl = tk.Label(search_frame, text="Search ", font="Arial 10")
+searchlbl = tk.Label(frames[0], text="Search ", font="Arial 10")
 searchlbl.place(x=50, y=300)
 
 # Drop Down menu
 listmenu = tk.StringVar() 
 listmenu.set(heading[0]) 
-drop = tk.OptionMenu(search_frame, listmenu, *heading) 
+drop = tk.OptionMenu(frames[0], listmenu, *heading) 
 drop.place(x=100, y=295, width=100)
 
-searchentry  = tk.Entry(search_frame)
+searchentry  = tk.Entry(frames[0])
 searchentry.place(x=220, y=300)
 
-searchbtn  = tk.Button(search_frame, text="Search", command=searchdb, bg="Gold")
+searchbtn  = tk.Button(frames[0], text="Search", command=searchdb, bg="Gold")
 searchbtn.place(x=350, y=300, height=20)
-
-
-
 
 root.mainloop()
