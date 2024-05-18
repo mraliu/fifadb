cabb2b344de9823549274ec8b3b5c1aa55ed6dea
mraliu <teachmrliu@gmail.com>
2024-05-18 10:19:55 +0100

    New pages on separate files

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import tkinter as tk, random
+from page1 import * 
 
 def getcolour():
     rgb = [random.randint(0,255), random.randint(0,255), random.randint(0,255)]
     return f'#{rgb[0]:02x}{rgb[1]:02x}{rgb[2]:02x}'
 
-def switchpage(pagenum):
+def switchpage(pagename):
     [frames[frame].place_forget() for frame in frames]
-    pageLbl.config(text=pagenum)
-    frames[pagenum].place(x=0,y=50, width=800, height=550)
+    pageLbl.config(text=pagename)
+    frames[pagename].place(x=0,y=50, width=800, height=550)
 
 # Tk Main Window
 root = tk.Tk()
@@ -15,8 +16,11 @@ root.title("DB window")
 root.geometry("800x600")
 
 # Pages stored in array
-pages = ["Homepage", "Products", "Checkout", "Help"]
+pages = ["Search", "Products", "Checkout", "Help"]
 frames = {page:tk.Frame(root, bg=getcolour()) for page in pages}
+
+# Initalise pages
+displayframe(frames["Search"])
 
 # File menu
 filemenu = tk.Menu(root)
